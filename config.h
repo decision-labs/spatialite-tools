f58a5eca5d3a0fd31ef3d7f4a08342e6bf66cf72
sandro <sandro@noemail.net>
2012-05-07 09:31:40 +0000

    supporting SPATIALITE_AMALGAMATION in ./configure (v.3.1.0a)
    FossilOrigin-Name: 08429c61a5b558b904d0a81e8eb008166ca93aa1

--- config.h
+++ config.h
@@ -126,7 +126,7 @@
 #define PACKAGE_NAME "spatialite-tools"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "spatialite-tools 3.1.0"
+#define PACKAGE_STRING "spatialite-tools 3.1.0a"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "spatialite-tools"
@@ -135,7 +135,10 @@
 #define PACKAGE_URL ""
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "3.1.0"
+#define PACKAGE_VERSION "3.1.0a"
+
+/* must be defined when using libspatialite-amalgamation */
+/* #undef SPATIALITE_AMALGAMATION */
 
 /* Define to 1 if you have the ANSI C header files. */
 #define STDC_HEADERS 1
@@ -147,7 +150,7 @@
 /* #undef TM_IN_SYS_TIME */
 
 /* Version number of package */
-#define VERSION "3.1.0"
+#define VERSION "3.1.0a"
 
 /* Define to empty if `const' does not conform to ANSI C. */
 /* #undef const */
